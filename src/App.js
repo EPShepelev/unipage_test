ef36a2756ba036bf1cca3fd752f9a3979957a7c8
Shepelev Evgeniy <shepelev.ep@yandex.ru>
2021-02-12 13:22:06 +0300

    installed icons; add icons components; imported it

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import "./App.css";
-import StartModal from "./components/StartModal";
+import StartModal from "./components/StartModal/StartModal";
+import Restart from "./components/Restart/Restart";
+import WrittingAccuracy from "./components/WrittingAccuracy/WrittingAccuracy";
+import WrittingSpeed from "./components/WrittingSpeed/WrittingSpeed";
 
 function App() {
   return (
     <div className="App">
       <StartModal />
+      <Restart />
+      <WrittingAccuracy />
+      <WrittingSpeed />
     </div>
   );
 }
